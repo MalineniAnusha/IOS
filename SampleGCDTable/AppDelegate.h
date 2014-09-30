b0e2ef75720436e5a4c3db7702eb7159a6a07c56
MalineniAnusha <pcs-mac@macs-Mac-mini.local>
2014-09-30 17:26:44 +0530

    gcd

---
+++ SampleGCDTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleGCDTable
+//
+//  Created by Chinna Venkata Raju A on 9/26/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleGCDTable/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SampleGCDTable
+//
+//  Created by Chinna Venkata Raju A on 9/26/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<NSURLConnectionDelegate>{
+    
+}
+
+@end
+
